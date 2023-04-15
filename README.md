cb59efe7fe7c7725cb051a9b9138c41d1f6fd2cd
nehldotjs <nelsonosuya11@gmail.com>
2023-04-15 23:07:40 +0100

    NELSON COMMITS

--- README.md
+++
@@ -1,1 +0,0 @@
-# Team_5-_Terra_Developers
--- js/script.js
+++
@@ -1,8 +0,0 @@
-// toggle the hamburger icon
-const hamburger = document.querySelector('.hamburger');
-const navList = document.querySelector('.navList');
-
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle("active");
-    navList.classList.toggle("active");
-})
