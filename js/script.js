96b9e2c632cf7dbb73ea0d97de0c53ce66de4e0d
Godfrey Offeh <103786881+Pyppersama@users.noreply.github.com>
2023-04-16 18:42:04 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle("active");
     navList.classList.toggle("active");
 })
+
+
+const perksBtn = document.getElementById("perksBtn");
+const perksListContainer = document.querySelector(".perksListWrapper");
+const perksArrow = document.getElementById("perksArrow");
+
+let isHidden = false;
+
+perksBtn.addEventListener("click", function () {
+  isHidden = !isHidden;
+  perksListContainer.classList.toggle("perksListWrapperHidden", isHidden);
+  perksArrow.classList.toggle("rotateArrow", isHidden);
+});
+
+console.log(isHidden);
+Footer
